547ad3e737f37862cab132410d70b8c068a3639d
padmajab2017 <31637027+padmajab2017@users.noreply.github.com>
2017-09-23 15:02:32 -0400

    Trying to commit first time

---
+++ createvisit/src/main/java/com/mmp/createvisit/App.java
@@ -0,0 +1,13 @@
+package com.mmp.createvisit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
